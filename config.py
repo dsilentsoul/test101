a99b07ccefa3fe4062e062ff629f84b90eb175ab
Mainak Sanyal <sanyalmainak97@gmail.com>
2021-05-09 08:47:52 +0530

    Update config.py
    Update config.py with Microsoft app id and pswd

--- config.py
+++ config.py
@@ -11,5 +11,5 @@ class DefaultConfig:
     """ Bot Configuration """
 
     PORT = 3978
-    APP_ID = os.environ.get("MicrosoftAppId", "")
-    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "")
+    APP_ID = os.environ.get("MicrosoftAppId", "5bc7445e-9c75-4613-a6cc-3ea3a9f6a46c")
+    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "Hf41wRuD21~w_3UA-PF8s~b12x1fx.-p2")
